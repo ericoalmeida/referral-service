9f0e2373c1dae086b3669400c65b39c30cebc183
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-11-28 17:02:02 -0400

    feat: add routeAdapter in health endpoint

--- src/main/routes/health.route.ts
+++ src/main/routes/health.route.ts
@@ -1,13 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 
+import { expressRouteAdapter } from '@main/adapters/express-route.adapter'
 import { serverEndpointsConstants } from '@main/constants'
+import { HealthController } from '@presentation/controllers'
 
 const healthRoute = Router()
 
-healthRoute.get(serverEndpointsConstants.healthCheck, (request, response) => {
-  return response.json({})
-})
+healthRoute.get(serverEndpointsConstants.healthCheck, expressRouteAdapter(new HealthController()))
 
-export {
-  healthRoute
-}
+export { healthRoute }
