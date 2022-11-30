8fd19ffe05a94f87b01eb3341653fbe11991dbc2
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-11-30 06:49:04 -0400

    chore: add paths

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,10 @@ module.exports = {
   testEnvironment: 'node',
   transform: { '.+\\.ts$': 'ts-jest' },
   moduleNameMapper: {
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+    '@main/(.*)': '<rootDir>/src/main/$1',
     '@presentation/(.*)': '<rootDir>/src/presentation/$1',
     '@tests/(.*)': '<rootDir>/tests/$1'
   }
