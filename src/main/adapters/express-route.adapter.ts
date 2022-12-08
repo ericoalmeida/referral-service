8381c75165c36e4b4ee5a606ef6659a087698b27
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-08 19:52:31 -0400

    refactor: turn expressAdapter into a single function

--- src/main/adapters/express-route.adapter.ts
+++ src/main/adapters/express-route.adapter.ts
@@ -2,17 +2,13 @@ import { Request, Response } from 'express'
 
 import { ControllerProtocol } from '@presentation/protocols'
 
-const buildRequestData = (request: Request): any => {
-  const bodyData = request.body !== undefined ? request.body : {}
-  const paramsData = request.params !== undefined ? request.params : {}
-  const queryData = request.query !== undefined ? request.query : {}
-
-  return { ...bodyData, ...paramsData, ...queryData }
-}
-
 const expressRouteAdapter = (controller: ControllerProtocol<any, any>) => {
   return async (request: Request, response: Response): Promise<Response<any, Record<any, any>>> => {
-    const requestData = buildRequestData(request)
+    const requestData = {
+      ...(request.body || {}),
+      ...(request.params || {}),
+      ...(request.query || {})
+    }
 
     const httpResponse = await controller.handle(requestData)
 
