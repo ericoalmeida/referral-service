f1bea61149505e9e1b970196b4d17a53a6a2c6f7
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-08 22:10:40 -0400

    chore: add contentType middleware

--- src/main/configs/setup-middlewares.config.ts
+++ src/main/configs/setup-middlewares.config.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express'
 
-import { bodyParserMiddleware, corsMiddleware } from '@main/middlewares'
+import { bodyParserMiddleware, contentTypeMiddleware, corsMiddleware } from '@main/middlewares'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParserMiddleware)
+  app.use(contentTypeMiddleware)
   app.use(corsMiddleware)
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,2 +1,3 @@
 export * from './body-parses.middleware'
+export * from './content-type.middleware'
 export * from './cors.middleware'
