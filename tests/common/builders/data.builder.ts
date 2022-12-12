f0cccea5f8b48f4c060425146cd89060143038eb
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-11 23:15:22 -0400

    feat: add base data builder

---
+++ tests/common/builders/data.builder.ts
@@ -0,0 +1,35 @@
+const attributeSeparator = '.'
+
+interface Data {
+  [key: string]: any
+}
+
+abstract class DataBuilder<T> {
+  protected data!: Data
+
+  public with (attribute: string, value: any): this {
+    const keysAttributes = attribute.split(attributeSeparator)
+
+    let keys = this.data
+
+    keysAttributes.forEach((key, index, list) => {
+      if (index === list.length - 1) {
+        keys[key] = value
+
+        return
+      }
+
+      keys = keys[key]
+    })
+
+    return this
+  }
+
+  public buildMany (length: number): T[] {
+    return Array.from({ length }, this.build.bind(this))
+  }
+
+  public abstract build (): T
+}
+
+export { DataBuilder, Data }
