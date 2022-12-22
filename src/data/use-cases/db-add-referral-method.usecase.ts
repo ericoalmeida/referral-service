b759a07f81c1dc33b9909469091436d17a7ae7f9
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-22 14:48:27 -0400

    feat: create referral methods to save then to the db

--- src/data/use-cases/db-add-referral-method.usecase.ts
+++ src/data/use-cases/db-add-referral-method.usecase.ts
@@ -1,12 +1,23 @@
+import { CreateReferralCodeProtocol } from '@data/protocols/create-referral-code.protocol'
+import { CreateReferralLinkProtocol } from '@data/protocols/create-referral-link.protocol'
 import { AddReferralMethodRepository } from '@data/protocols/repositories/add-referral-method.repository'
-import { AddReferralMethodParams } from '@domain/params'
-import { AddReferralMethodUseCase } from '@domain/use-cases'
+import { AddReferralMethodParams } from '@domain/params/add-referral-method.params'
+import { AddReferralMethodUseCase } from '@domain/use-cases/add-referral-method.usecase'
 
 class DbAddReferralMethodUseCase implements AddReferralMethodUseCase {
-  constructor (private readonly repository: AddReferralMethodRepository) {}
+  constructor (
+    private readonly referralCode: CreateReferralCodeProtocol,
+    private readonly referralLink: CreateReferralLinkProtocol,
+    private readonly addReferralMethodRepository: AddReferralMethodRepository
+  ) {}
 
   async add (params: AddReferralMethodParams): Promise<void> {
-    await this.repository.add(params)
+    const { user_id } = params
+
+    const code = this.referralCode.createCode()
+    const link = this.referralLink.createLink()
+
+    await this.addReferralMethodRepository.add({ user_id, code, link })
   }
 }
 
