f894ea60dd82da394e1e70e83415c7214e7a4ffb
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-24 03:32:04 -0400

    test: change tests for health route

--- tests/presentation/controllers/health-controller.spec.ts
+++ tests/presentation/controllers/health-controller.spec.ts
@@ -5,25 +5,32 @@ import { HealthControllerFactory } from '@tests/presentation/factories'
 
 describe('HealthController', () => {
   describe('#handle', () => {
+    const expectedUseCaseCallTimes = 1
+
     it('Should return successful response with status code 204/no-content', async () => {
-      const { sut } = new HealthControllerFactory()
+      const { sut, useCase } = new HealthControllerFactory()
+
+      const useCaseSpy = jest.spyOn(useCase, 'check')
 
       const response = await sut.handle({})
 
+      expect(useCaseSpy).toHaveBeenCalledTimes(expectedUseCaseCallTimes)
       expect(response.statusCode).toBe(httpStatusCodeConstants.successful.noContent)
     })
 
     it('Should return failure response with status code 500/internal-server-error', async () => {
-      const { sut, health } = new HealthControllerFactory()
+      const { sut, useCase } = new HealthControllerFactory()
 
-      jest.spyOn(health, 'check').mockImplementationOnce(async () => {
-        throw new Error()
-      })
+      const useCaseSpy = jest.spyOn(useCase, 'check').mockRejectedValue(new Error())
 
       const response = await sut.handle({})
 
-      expect(response.statusCode).toBe(httpStatusCodeConstants.serverError.internalServerError)
-      expect(response).toEqual(httpInternalServerError(errorCodesConstants.healthCheckFailure))
+      const { serverError } = httpStatusCodeConstants
+      const { healthCheckFailure } = errorCodesConstants
+
+      expect(useCaseSpy).toHaveBeenCalledTimes(expectedUseCaseCallTimes)
+      expect(response.statusCode).toBe(serverError.internalServerError)
+      expect(response).toEqual(httpInternalServerError(healthCheckFailure))
     })
   })
 })
--- tests/presentation/factories/health-controller.factory.ts
+++ tests/presentation/factories/health-controller.factory.ts
@@ -1,17 +1,12 @@
 import { HealthController } from '@presentation/controllers'
-import {
-  ControllerProtocol,
-  HealthRequestProtocol,
-  HealthResponseProtocol
-} from '@presentation/protocols'
 
 import { HealthUseCaseStub } from '@tests/presentation/stubs'
 
 class HealthControllerFactory {
-  public health = new HealthUseCaseStub()
+  public useCase = new HealthUseCaseStub()
 
-  public get sut (): ControllerProtocol<HealthRequestProtocol, HealthResponseProtocol> {
-    return new HealthController(this.health)
+  public get sut (): HealthController {
+    return new HealthController(this.useCase)
   }
 }
 
