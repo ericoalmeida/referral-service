eb4983b02ff46beba9cda471f23e172dc704affb
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-25 08:45:48 -0400

    feat: add endpoint for find referral method

--- src/main/routes/referral-method.route.ts
+++ src/main/routes/referral-method.route.ts
@@ -3,12 +3,19 @@ import { Router } from 'express'
 import { expressRouteAdapter } from '@main/adapters/express-route.adapter'
 import { serverEndpointsConstants } from '@main/constants/server-endpoints.constants'
 import { addReferralMethodControllerFactory } from '@main/factories/controllers/add-referral-method-controller.factory'
+import { findReferralMethodControllerFactory } from '@main/factories/controllers/find-referral-method-controller.factory'
 
 const referralMethodRoute = Router()
 
+const { referralMethod } = serverEndpointsConstants
+
 referralMethodRoute.post(
-  serverEndpointsConstants.referralMethod.add,
+  referralMethod.add,
   expressRouteAdapter(addReferralMethodControllerFactory())
+)
+referralMethodRoute.get(
+  referralMethod.find,
+  expressRouteAdapter(findReferralMethodControllerFactory())
 )
 
 export { referralMethodRoute }
