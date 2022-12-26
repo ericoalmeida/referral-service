19d6c7fba21ee548b772e14ce2585d31ff3f2eaa
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-25 21:46:23 -0400

    feat: add environment variables

--- src/main/configs/environments.config.ts
+++ src/main/configs/environments.config.ts
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv'
 
+import { applicationEnvironmentsConstants } from '@main/constants/application-environments.constants'
+
 dotenv.config()
 
+const { development } = applicationEnvironmentsConstants
+
+const applicationEnvironment = process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : development
 const port = process.env.PORT !== undefined ? process.env.PORT : 7000
 const referralCodeSize = 8
 
 export const environmentsConfig = {
+  applicationEnvironment,
   port,
   referralCodeSize
 }
