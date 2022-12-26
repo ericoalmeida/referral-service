9b916c4b4b48adf80e3508f43bd68942fa7088c9
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-25 22:18:01 -0400

    docs: add HealthCheck endpoint documentation

--- src/main/routes/health.route.ts
+++ src/main/routes/health.route.ts
@@ -6,6 +6,37 @@ import { healthControllerFactory } from '@main/factories/controllers/health-cont
 
 const healthRoute = Router()
 
+/**
+ * @swagger
+ * /api/health:
+ *   get:
+ *     tags:
+ *       - Health
+ *     summary: Server health check!
+ *     description: Check application is ready!
+ *     responses:
+ *       204:
+ *         description: Application is ready.
+ *       500:
+ *         description: Applications is not ready
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  error:
+ *                     type: object
+ *                     properties:
+ *                        type:
+ *                          type: object
+ *                          properties:
+ *                            code:
+ *                              type: string
+ *                              example: HC0001
+ *                            message:
+ *                              type: string
+ *                              example: Health check failed
+ */
 healthRoute.get(serverEndpointsConstants.healthCheck, expressRouteAdapter(healthControllerFactory()))
 
 export { healthRoute }
