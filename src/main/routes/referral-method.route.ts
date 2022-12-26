52ae8c3e9647fbbe41d0a030f093b06f57793e65
Erico Almeida <ericoalmeida.egr@gmail.com>
2022-12-26 12:53:08 -0400

    docs: add referral method endpoint docs

--- src/main/routes/referral-method.route.ts
+++ src/main/routes/referral-method.route.ts
@@ -9,6 +9,59 @@ const referralMethodRoute = Router()
 
 const { referralMethod } = applicationEndpointsConstants
 
+/**
+ * @swagger
+ * /api/referral-method:
+ *   post:
+ *     tags:
+ *       - Referral Method
+ *     summary: Creates a referral method
+ *     description: Creates a referral method
+ *     requestBody:
+ *      description: Creates a referral method properties
+ *      content:
+ *        application/json:
+ *          schema:
+ *            required:
+ *              - user_id
+ *            type: object
+ *            properties:
+ *              user_id:
+ *                type: string
+ *                format: uuid
+ *              code:
+ *                type: string
+ *              link:
+ *                type: string
+ *      required: true
+ *     responses:
+ *       204:
+ *         description: Application is ready
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                data:
+ *                  type: object
+ *       500:
+ *         description: Applications is not ready
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                  error:
+ *                     type: object
+ *                     properties:
+ *                        type:
+ *                          type: object
+ *                          properties:
+ *                            code:
+ *                              type: string
+ *                            message:
+ *                              type: string
+ */
 referralMethodRoute.post(
   referralMethod.add,
   expressRouteAdapter(addReferralMethodControllerFactory())
