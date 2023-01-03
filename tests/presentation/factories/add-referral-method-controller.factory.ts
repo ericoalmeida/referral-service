418911045300a92d478981c5eb8a082160bc300c
Erico Almeida <ericoalmeida.egr@gmail.com>
2023-01-03 08:48:22 -0400

    test: add test to validate required fields

--- tests/presentation/factories/add-referral-method-controller.factory.ts
+++ tests/presentation/factories/add-referral-method-controller.factory.ts
@@ -1,12 +1,15 @@
 import { AddReferralMethodController } from '@presentation/controllers/add-referral-method.controller'
 
 import { AddReferralMethodUseCaseStub } from '@tests/presentation/stubs/add-referral-method-use-case.stub'
+import { ValidationStub } from '@tests/presentation/stubs/validation.stub'
 
 class AddReferralMethodControllerFactory {
   public useCase = new AddReferralMethodUseCaseStub()
 
+  public validation = new ValidationStub()
+
   public get sut (): AddReferralMethodController {
-    return new AddReferralMethodController(this.useCase)
+    return new AddReferralMethodController(this.useCase, this.validation)
   }
 }
 
